d3904f530d1684c76a5a42984e8dfe5a617fcfb7
Muhammad Rizky Wijaya <email.muhammadrizkywijaya003@gmail.com>
2026-07-03 00:38:09 +0800

    fix: perbaiki sintaks kurung pada dialog grafik dan model sensor

--- lib/models/sensor_model.dart
+++ lib/models/sensor_model.dart
@@ -4,6 +4,7 @@ class SensorData {
   final double phTanah;
   final double debitAir;
   final bool statusHujan;
+  final String waktuRekam;
 
   SensorData({
     required this.kelembapanTanah,
@@ -11,6 +12,7 @@ class SensorData {
     required this.phTanah,
     required this.debitAir,
     required this.statusHujan,
+    required this.waktuRekam,
   });
 
   factory SensorData.fromJson(Map<String, dynamic> json) {
@@ -20,6 +22,20 @@ class SensorData {
       phTanah: (json['ph_tanah'] as num).toDouble(),
       debitAir: (json['debit_air'] as num).toDouble(),
       statusHujan: json['status_hujan'] as bool,
+      waktuRekam: json['waktu_rekam'] != null ? strToTime(json['waktu_rekam'].toString()) : 'Baru saja',
     );
+  }
+
+  static String strToTime(String str) {
+    try {
+      if (str.contains('T')) {
+        final parts = str.split('T');
+        final timePart = parts[1].split('.')[0];
+        return "${parts[0]} $timePart";
+      }
+      return str;
+    } catch (_) {
+      return str;
+    }
   }
 }
